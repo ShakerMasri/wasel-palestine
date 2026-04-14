d223b04f944bfd14ae496e35aa9b3bbc2bd90031
Mohammed Maradwi <mohammadkhaledmaradwe@gmail.com>
2026-04-14 17:50:21 +0300

    feat: add ReportVote and ModerationLog entities to the TypeORM configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { ReportsModule } from './reports/reports.module'; // 1. استيراد �
 import { User } from './auth/entities/user.entity';
 import { Incident } from './reports/entities/incident.entity';
 import { UserReport } from './reports/entities/user-report.entity';
+import { ReportVote } from './reports/entities/report-vote.entity';
+import { ModerationLog } from './reports/entities/moderation-log.entity';
 import { Checkpoint } from './checkpoints/entities/checkpoint.entity';
 import { CheckpointHistory } from './checkpoints/entities/CheckpointHistory.entity';
 
@@ -18,7 +20,15 @@ import { CheckpointHistory } from './checkpoints/entities/CheckpointHistory.enti
       username: 'admin_wasel',
       password: 'wasel1234',
       database: 'wasel_palestine',
-      entities: [User, Incident, Checkpoint, CheckpointHistory, UserReport],
+      entities: [
+        User,
+        Incident,
+        Checkpoint,
+        CheckpointHistory,
+        UserReport,
+        ReportVote,
+        ModerationLog,
+      ],
       synchronize: true,
     }),
     AuthModule,
