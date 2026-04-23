9eb1d8ef01ec0e45ab49545ded9e8b357fab5ea6
ShakerMasri <playerg386@gmail.com>
2026-04-23 20:47:52 +0300

    added swagger

--- src/route-mobility/dto/update-report-status.dto.ts
+++ src/route-mobility/dto/update-report-status.dto.ts
@@ -1,6 +1,11 @@
 import { IsEnum, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateReportStatusDto {
+  @ApiProperty({
+    example: 'In Progress',
+    enum: ['Open', 'In Progress', 'Resolved', 'Closed'],
+  })
   @IsEnum(['Open', 'In Progress', 'Resolved', 'Closed'], {
     message: 'Report status is invalid',
   })
